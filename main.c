622b96de69ad192792f64a8906af0eb7d85486e8
Hyykk <dbsrydia@kw.ac.kr>
2025-03-25 14:34:44 +0900

    초기 커밋.

---
+++ main.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_image.h>
+
+int main(void) {
+
+	al_init();
+
+	return 0;
+}
