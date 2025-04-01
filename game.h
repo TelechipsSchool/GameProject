824479bc1251cfaefa43c975fc0811c0a3300ea6
Hyykk <dbsrydia@kw.ac.kr>
2025-04-01 15:07:13 +0900

    [feat] 중력계수 수정

--- game.h
+++ game.h
@@ -31,8 +31,8 @@ float background_interval = 0.2f;
 
 // 물리 계수 (수정 시 꼭 말하고 하기)
 double GRAVITY = 9.8f;  // 중력
-double centerCoefficient = 0.015f;
-double RESTITUTION = 0.6f;      // 탄성 계수
+double centerCoefficient = 0.03f;
+double RESTITUTION = 0.3f;      // 탄성 계수
 double dragForce = 150.0f;      // 당기는 힘
 double maxForce = 200.0f;      // 최대 당기는 힘
 
