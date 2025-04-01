4fc1abeb7ddc6252d2b2c412db3dee970ece3032
Hyykk <dbsrydia@kw.ac.kr>
2025-04-01 17:12:55 +0900

    [font] 폰트 변경

--- font.c
+++ font.c
@@ -1,54 +1,56 @@
 #include "font.h"
 
+#define FONT_PATH "fonts/ChunkFive-Regular.otf"
+
 ALLEGRO_FONT* get_title_font() {
-	ALLEGRO_FONT* font = al_load_font("fonts/D2Coding/D2CodingBold-Ver1.3.2-20180524.ttf", 140, 0);
+	ALLEGRO_FONT* font = al_load_font(FONT_PATH, 140, 0);
 	return font;
 }
 
 ALLEGRO_FONT* get_menu_font() {
 	// 폰트
-	ALLEGRO_FONT* font = al_load_font("fonts/D2Coding/D2CodingBold-Ver1.3.2-20180524.ttf", 50, 0);
+	ALLEGRO_FONT* font = al_load_font(FONT_PATH, 50, 0);
 	return font;
 }
 
 ALLEGRO_FONT* get_next_font() {
 	// 폰트
-	ALLEGRO_FONT* font = al_load_font("fonts/D2Coding/D2CodingBold-Ver1.3.2-20180524.ttf", 30, 0);
+	ALLEGRO_FONT* font = al_load_font(FONT_PATH, 30, 0);
 	return font;
 }
 
 ALLEGRO_FONT* get_score_text_font() {
 	// 폰트
-	ALLEGRO_FONT* font = al_load_font("fonts/D2Coding/D2CodingBold-Ver1.3.2-20180524.ttf", 30, 0);
+	ALLEGRO_FONT* font = al_load_font(FONT_PATH, 30, 0);
 	return font;
 }
 
 ALLEGRO_FONT* get_score_font() {
 	// 폰트
-	ALLEGRO_FONT* font = al_load_font("fonts/D2Coding/D2CodingBold-Ver1.3.2-20180524.ttf", 40, 0);
+	ALLEGRO_FONT* font = al_load_font(FONT_PATH, 40, 0);
 	return font;
 }
 
 ALLEGRO_FONT* get_score_best_font() {
 	// 폰트
-	ALLEGRO_FONT* font = al_load_font("fonts/D2Coding/D2CodingBold-Ver1.3.2-20180524.ttf", 20, 0);
+	ALLEGRO_FONT* font = al_load_font(FONT_PATH, 20, 0);
 	return font;
 }
 
 ALLEGRO_FONT* get_username_font() {
 	// 폰트
-	ALLEGRO_FONT* font = al_load_font("fonts/D2Coding/D2CodingBold-Ver1.3.2-20180524.ttf", 30, 0);
+	ALLEGRO_FONT* font = al_load_font(FONT_PATH, 30, 0);
 	return font;
 }
 
 ALLEGRO_FONT* get_entername_font() {
 	// 폰트
-	ALLEGRO_FONT* font = al_load_font("fonts/D2Coding/D2CodingBold-Ver1.3.2-20180524.ttf", 50, 0);
+	ALLEGRO_FONT* font = al_load_font(FONT_PATH, 50, 0);
 	return font;
 }
 
 ALLEGRO_FONT* get_rank_font() {
 	// 폰트
-	ALLEGRO_FONT* font = al_load_font("fonts/D2Coding/D2CodingBold-Ver1.3.2-20180524.ttf", 50, 0);
+	ALLEGRO_FONT* font = al_load_font(FONT_PATH, 50, 0);
 	return font;
 }
