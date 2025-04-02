c5e835271b8eb1b2397fee131a1014dc6ef92d2d
inbangCert <bansong6@naver.com>
2025-04-02 17:28:18 +0900

    story skip 추가

--- story.c
+++ story.c
@@ -1,39 +1,46 @@
 ﻿#include "story.h"
+bool skip_story = false;
+
+void wait_or_skip(double duration) {
+    double start = al_get_time();
+    while (al_get_time() - start < duration) {
+        ALLEGRO_KEYBOARD_STATE key_state;
+        al_get_keyboard_state(&key_state);
+        if (al_key_down(&key_state, ALLEGRO_KEY_Q)) {
+            skip_story = true;
+            break;
+        }
+        al_rest(0.01);
+    }
+}
+
 
 void story1() {
     // 게임 시작 시 스토리
 
     // 배경 화면 생성
     ALLEGRO_BITMAP* story_screen = load_bitmap_resized("images/setting.png", SCREEN_W, SCREEN_H);
-    if (!story_screen) {
+    if (!story_screen || !display) {
         DEBUG_MSG(story_screen - 화면 로드 실패);
         return 0;
     }
     al_draw_scaled_bitmap(story_screen, 0, 0, al_get_bitmap_width(story_screen), al_get_bitmap_height(story_screen), 0, 0, SCREEN_W, SCREEN_H, 0);
-    
-    // 캐릭터 생성
-    ALLEGRO_BITMAP* a_play = load_bitmap_resized("images/astronaut/play.png", 400, 400);
-    if (!a_play) {
-        DEBUG_MSG(story_screen - 화면 로드 실패);
-        return 0;
-    }
 
     int y_position = 100;
 
     // 텍스트 시작
-    al_rest(2.0);
-    al_draw_bitmap(a_play, 1200, 400, 0);
+    wait_or_skip(2.0);
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"안녕, 난 암스트롱이야");
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"너의 이름은 뭐야?");
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, SCREEN_H - 100, ALLEGRO_ALIGN_CENTER, u8"<<   Enter를 눌러서 다음으로 이동  >>");
     al_flip_display();
@@ -56,12 +63,12 @@ void story1() {
 
     // 텍스트 위에서부터 다시 시작
     y_position = 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
     al_draw_textf(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"반가워 %s!", username);
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"지구가 점점 온난화로 인해 살기 힘든 환경이 되어가고 있어.");
     y_position += 100;
@@ -69,7 +76,7 @@ void story1() {
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"다행히도, 다른 은하계에서 살 수 있는 행성을 찾았어.");
     y_position += 100;
@@ -77,7 +84,7 @@ void story1() {
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"우리의 첫 번째 목표는, 주변 행성들을 융합해서 태양을 만드는 거야.");
     y_position += 100;
@@ -85,7 +92,7 @@ void story1() {
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, SCREEN_H - 100, ALLEGRO_ALIGN_CENTER, u8"<<   Enter를 눌러서 게임 시작  >>");
     al_flip_display();
@@ -100,8 +107,8 @@ void story1() {
         al_flip_display();
     }
 
-	// 게임 진행
-	start_game(username);
+    // 게임 진행
+    start_game(username);
 }
 
 void story2() {
@@ -118,14 +125,14 @@ void story2() {
     int y_position = 100;
 
     // 텍스트 시작
-    al_rest(2.0);
+    wait_or_skip(2.0);
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"좋아! 드디어 태양을 만들었어.");
     y_position += 100;
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"이제 우리는 그 행성에서 살아갈 수 있어!");
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"하지만, 문제가 있어. 그 행성으로 소행성이 다가오고 있어.");
     y_position += 100;
@@ -133,7 +140,7 @@ void story2() {
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"이제 소행성을 부수고, 행성을 지켜야 해! 이건 우리에게 중요한 전투야!");
     y_position += 100;
@@ -141,7 +148,7 @@ void story2() {
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, SCREEN_H - 100, ALLEGRO_ALIGN_CENTER, u8"<<   Enter를 눌러서 게임 시작  >>");
     al_flip_display();
@@ -175,14 +182,14 @@ void story3() {
     int y_position = 100;
 
     // 텍스트 시작
-    al_rest(2.0);
+    wait_or_skip(2.0);
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"뭐야? 외계 적들이 나타났어!");
     y_position += 100;
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"이들은 우리가 행성을 지키는 걸 방해하려고 하는 놈들이야.");
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"이제 우리가 외계 적들과 싸워야 할 시간이야.");
     y_position += 100;
@@ -190,7 +197,7 @@ void story3() {
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, SCREEN_H - 100, ALLEGRO_ALIGN_CENTER, u8"<<   Enter를 눌러서 게임 재개  >>");
     al_flip_display();
@@ -211,7 +218,7 @@ void story3() {
 
 void story4(char* username) {
     // 게임 종료 후 스토리
-    
+
     // 배경 화면 생성
     ALLEGRO_BITMAP* story_screen = load_bitmap_resized("images/setting.png", SCREEN_W, SCREEN_H);
     if (!story_screen) {
@@ -223,14 +230,14 @@ void story4(char* username) {
     int y_position = 100;
 
     // 텍스트 시작
-    al_rest(2.0);
+    wait_or_skip(2.0);
     al_draw_textf(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"해냈어, %s 소행성도, 외계 적들도 모두 물리쳤어!", username);
     y_position += 100;
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"이제 우리는 그 행성에서 살아갈 수 있어!");
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"이제 이곳은 우리가 살아갈 새로운 고향이 될 거야.");
     y_position += 100;
@@ -238,7 +245,7 @@ void story4(char* username) {
     play_music("audio/story.ogg");
     al_flip_display();
     y_position += 100;
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_textf(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, y_position, ALLEGRO_ALIGN_CENTER, u8"넌 정말 대단해, %s! 너 없었으면 우린 이겨내지 못했을 거야.", username);
     y_position += 100;
@@ -255,7 +262,7 @@ void story4(char* username) {
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, SCREEN_H - 200, ALLEGRO_ALIGN_CENTER, u8"넌 정말 대단해, <이름>! 너 없었으면 우린 이겨내지 못했을 거야.");
     play_music("audio/story.ogg");
     al_flip_display();
-    al_rest(2.0);
+    wait_or_skip(2.0);
 
     al_draw_text(story_font, al_map_rgb(255, 255, 255), SCREEN_W / 2, SCREEN_H - 100, ALLEGRO_ALIGN_CENTER, u8"<<   Enter를 눌러서 게임 종료  >>");
     al_flip_display();
@@ -339,3 +346,7 @@ char* getUserName() {
 
     return username;  // 동적 할당된 메모리를 반환
 }
+
+
+
+
