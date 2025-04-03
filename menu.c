2dfd84240f5bd18ab63b8aff8effed159b488820
Hyykk <dbsrydia@kw.ac.kr>
2025-04-03 09:10:34 +0900

    스토리 마무리

--- menu.c
+++ menu.c
@@ -73,7 +73,7 @@ void menu() {
                     al_stop_samples();
                     play_music("audio/enter.ogg");
                     if (selected == MENU_START) {
-                        story4("yoonkyo");
+                        story1();
                     }
                     else if (selected == MENU_RANK) display_ranks();
                     else if (selected == MENU_SETTING) {
